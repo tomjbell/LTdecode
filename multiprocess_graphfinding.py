97eb3ef9dd3662dde1929a02d18426d3fd5e9c4d
tom <tj.bell144@gmail.com>
2022-07-27 16:28:41 +0100

    improved batch processing with periodic savedata

--- multiprocess_graphfinding.py
+++ multiprocess_graphfinding.py
@@ -1,45 +1,35 @@
 from multiprocessing import Pool, cpu_count
-from decoder_class import CascadeDecoder
+from decoder_class import CascadeDecoder, FastDecoder
 from os import getcwd
 from networkx import Graph
 from time import time
 from helpers import load_obj, save_obj
 from numpy import array_split
+from logical_fusions import AdaptiveFusionDecoder
+from graph_finding import permute_input_qubit, txt_to_edge_data, gen_non_isomorphic_graphs
 
 
-def graph_performance(n, lc_equiv=False, save_perf_dicts=False, mp=False, cascading=True):
-    from helpers import load_obj, save_obj
-    path = getcwd() + '/LC_equiv_graph_data'
+def graph_performance_fast(n, chunk_size=None):
+    # Do batches of 100 graphs
+    graph_list = txt_to_edge_data(f'{n}_qubit_graphs_data_uib.txt')
+    all_graphs_data = []
+    for e in graph_list:
+        all_graphs_data += gen_non_isomorphic_graphs(e)
+
+    graph_data = all_graphs_data
+    if chunk_size is None:
+        cpus = cpu_count()
+        n_graphs = len(graph_data)
+        all_graph_ixs = list(range(n_graphs))
+        input_lists = [list(x) for x in array_split(all_graph_ixs, cpus)]
+        graph_lists = [[graph_data[ix] for ix in input_lists[i]] for i in range(cpus)]  # Take only the first from each equivalence class
+    else:
+        graph_lists = [graph_data[i:i + chunk_size] for i in range(0, len(graph_data), chunk_size)]
+
+    return graph_lists
 
 
-    graph_data = load_obj(f'{n}QubitCi', path)
-    n_classes = len(graph_data)
-    c = 1
-    results = []
-
-    if not lc_equiv:
-        for lc_class in graph_data:
-            print(f'inspecting graph {c}/{n_classes}, number of qubits = {n}')
-            c += 1
-            res = []
-            edge_list = lc_class[0]  # Just choose the first graph
-            res.append(edge_list)
-            g = Graph()
-            g.add_nodes_from([i for i in range(n)])
-            g.add_edges_from(edge_list)
-
-            x = CascadeDecoder(g)
-            bases = ['spc', 'x', 'y', 'z', 'xy']
-            spc, xr, yr, zr, xyr = [x.get_dict(basis=b) for b in bases]
-            for mr in spc, xr, yr, zr, xyr:
-                res.append(mr)
-            results.append(res)
-        if save_perf_dicts:
-            save_obj(results, f'{n}QubitResultDicts', path)
-        return results
-
-
-def graph_pef_on_batch(graph_list):
+def graph_perf_on_batch(graph_list):
     results = []
     c = 1
     for k in graph_list:
@@ -49,44 +39,46 @@ def graph_pef_on_batch(graph_list):
     return results
 
 
+def graph_perf_logical_fusion(edges):
+    g = Graph()
+    g.add_edges_from(edges)
+    x = AdaptiveFusionDecoder(g)
+    x.build_tree()
+    return x.get_threshold(pfail=0.5)
+
+
 def graph_perf(edges):
     g = Graph()
-    g.add_nodes_from(list(range(7)))
+    n = max([i for edge in edges for i in edge])
+    g.add_nodes_from(list(range(n)))
+
     # print(edges)
     g.add_edges_from(edges)
 
-    x = CascadeDecoder(g)
-    bases = ['spc', 'x', 'y', 'z', 'xy']
-    spc, xr, yr, zr, xyr = [x.get_dict(basis=b) for b in bases]
-    return edges, spc, xr, yr, zr, xyr
+    x = FastDecoder(g)
+    spc = x.get_dict()
+    return edges, spc
 
 
 if __name__ == '__main__':
-    t0 = time()
-    n = 8
-    lc_equiv = False
-    save_perf_dicts = False
-    mp = False
-    cascading = False
-    save = False
-    n_threads = cpu_count()
+    n=10
 
-    path = getcwd() + '/LC_equiv_graph_data'
-    graph_data = load_obj(f'{n}QubitCi', path)
-    n_classes = len(graph_data)
-    all_graph_ixs = list(range(n_classes))
-    if n_classes % n_threads:
-        per_thread = n_classes // n_threads + 1
+    # Do first 11 x 100
+    n_cpu = cpu_count() - 1
+    graph_lists = graph_performance_fast(n, chunk_size=20)
+    n_chunks = len(graph_lists)
+    if n_chunks % n_cpu:
+        n_batches = n_chunks // n_cpu + 1
     else:
-        per_thread = n_classes / n_threads
-    input_lists = [list(x) for x in array_split(all_graph_ixs, n_threads)]
-    print(n_classes, per_thread, n_threads)
-    print(input_lists)
-    graph_lists = [[graph_data[ix][0] for ix in input_lists[i]] for i in range(n_threads)]  # Take only the first from each equivalence class
-    print(graph_lists)
+        n_batches = n_chunks / n_cpu
 
-    with Pool(n_threads) as p:
-        out = p.map(graph_pef_on_batch, graph_lists)
-    t1 = time()
-    print(out)
-    print(t1 - t0)
+    for i in range(1, n_batches):
+        if i < n_batches - 1:
+            batch = graph_lists[i * n_cpu:(i+1) * n_cpu]
+        else:
+            batch = graph_lists[(n_batches - 1) * n_cpu:]
+
+        with Pool(11) as p:
+            out = p.map(graph_perf_on_batch, batch)
+
+        save_obj(out, f'{n}QubitResultsDicts_PermuteInputFastDecoder_batch{i}', getcwd() + '/graph_perf_10q')
