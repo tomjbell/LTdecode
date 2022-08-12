8aaa2c79d9bf221103ea07b8e768c908992907cd
tom <tj.bell144@gmail.com>
2022-08-12 18:31:24 +0100

    Added launch scripts for 12 qubit graphs

---
+++ run_graph_performance_search_100.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --job-name=graphs_12q_100_batches
+#SBATCH --cpus-per-task=28
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=2:00:00
+#SBATCH --mem-per-cpu=1000M
+#SBATCH --array=0-99
+
+module load languages/anaconda3/2021.09-3.9.7-tflow.1.12-deeplabcut
+date
+python3 gen_12_data_batches.py -arix=${SLURM_ARRAY_TASK_ID} -p="$PWD/data/uib_data" -od="$PWD/graph_data_12q" -df="entanglement12.bz2" -bs=500
+date
+
--- test_bc.sh
+++ test_bc.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --job-name=graphs_12q_test
+#SBATCH --cpus-per-task=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=0:10:00
+#SBATCH --mem-per-cpu=1000M
+#SBATCH --array=0-1
+#SBATCH --partition=test
 
-dirname=$(python3 generate_input_files.py -fn="10_qubit_graphs_ordered_num_in_class" -p="$PWD/data/uib_data" -bs=100)
-echo "$dirname"
-echo "Data generated"
-echo "analysing graphs"
-python3 script_blue_crystal.py -arix=7 -p="$PWD/$dirname"
-
+module load languages/anaconda3/2021.09-3.9.7-tflow.1.12-deeplabcut
+date
+python3 gen_12_data_batches.py -arix=${SLURM_ARRAY_TASK_ID} -p="$PWD/data/uib_data" -od="$PWD/graph_data_12q" -df="entanglement12.bz2" -bs=2
+date
