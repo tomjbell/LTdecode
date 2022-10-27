bc7433b9abb5ba255d3c221fea3022a6e7dd1cc9
tom <tj.bell144@gmail.com>
2022-10-27 15:50:32 +0100

    Added ec for concatenated codes. Removed old best checks function

--- error_correction.py
+++ error_correction.py
@@ -7,6 +7,77 @@ from networkx import from_numpy_matrix
 from networkx.algorithms.clique import find_cliques as maximal_cliques
 
 
+def concat_error_correction(targets, checks, meas_accuracies, output_q=None, printing=False):
+    """
+
+    :param targets:
+    :param checks:
+    :param meas_accuracies:
+    :param output_q:
+    :param printing:
+    :return:
+    """
+    # find the set of qubits that have been measured and are hence vulnerable to errors
+    total_paulis_done = multi_union(targets + checks)
+    qubits_with_error = total_paulis_done.support
+    n_err_q = len(qubits_with_error)
+
+    # Create a dictionary that tells us what type of measurement was done on each qubit, so we can assign probabilities
+    meas_type_to_ix = {'x': 0, 'y': 1, 'z': 2, 'spc': 3}
+    qubit_meas_dict = {}
+    for q in qubits_with_error:
+        if q == output_q:
+            meas_type = 'spc'
+        else:
+            meas_type = total_paulis_done.get_meas_type(q)
+        qubit_meas_dict[q] = meas_type_to_ix[meas_type]
+
+    # Find the probaility of a particular set of errors given the error model
+    def p_error_pattern(ixs):
+        prob_pattern = 1
+        for q in qubits_with_error:
+            if q in ixs:
+                prob_pattern *= 1 - meas_accuracies[qubit_meas_dict[q]]
+            else:
+                prob_pattern *= meas_accuracies[qubit_meas_dict[q]]
+        return prob_pattern
+
+    syndrome_prob_dicts = {}
+    synd_tot_probs = {}
+    win_prob = 0
+    for flip_pat in product((0, 1), repeat=n_err_q):
+        # 1 Find the probability
+        error_ixs = [qubits_with_error[i] for i in range(n_err_q) if flip_pat[i]]
+        prob = p_error_pattern(error_ixs)
+        # 2 Find the syndrome
+        syndrome = tuple([sum([x in ch.support for x in error_ixs]) % 2 for ch in checks])
+        # 3 Find the error pattern
+        error = tuple([sum([x in t.support for x in error_ixs]) % 2 for t in targets])
+        if printing:
+            print(f'{flip_pat=}')
+            print(f'{error_ixs=}')
+            print(f'{prob=}')
+            print(f'{syndrome=}')
+            print(f'{error=}')
+
+        if syndrome not in syndrome_prob_dicts.keys():
+            syndrome_prob_dicts[syndrome] = {x: 0 for x in product((0, 1), repeat=len(targets))}
+            synd_tot_probs[syndrome] = 0
+        syndrome_prob_dicts[syndrome][error] += prob
+        synd_tot_probs[syndrome] += prob
+    confidence = {}
+    for syndrome in syndrome_prob_dicts.keys():
+        max_value = max(syndrome_prob_dicts[syndrome].values())  # maximum value
+        confidences_dict = {k: v / synd_tot_probs[syndrome] for k, v in syndrome_prob_dicts[syndrome].items()}
+        confidence[syndrome] = confidences_dict
+        max_keys = [k for k, v in syndrome_prob_dicts[syndrome].items() if v == max_value]
+        # print(syndrome, max_keys)
+        win_prob += max_value
+    # print(f'{win_prob=}')
+    return win_prob, confidence
+
+
+
 def cascade_error_correction(targets, checks, p0, meas_accuracies, indirect_z_ixs=None, ignore_qs=None, lost_qubits=None,
                              printing=False):
     """
@@ -17,7 +88,6 @@ def cascade_error_correction(targets, checks, p0, meas_accuracies, indirect_z_ix
     :param checks: The set of Pauli check operators we will use to check the target operator
     :param p0: The bare measurement accuracy
     :param meas_accuracies: Accuracy of the [x, y, z, z_indirect] measurements
-    :param nzi: The number of indirect z measurements performed
     :param indirect_z_ixs: the indices of qubits with indirect z measurements performed on them
     :param ignore_qs:
     :param lost_qubits:
@@ -87,9 +157,11 @@ def cascade_error_correction(targets, checks, p0, meas_accuracies, indirect_z_ix
     return win_prob, confidence
 
 
-def pauli_error_decoder(targets, checks, ps, max_weight=2, ignore=(0,), lost_qubits=None, printing=False):
+def pauli_error_decoder(targets, checks, ps, max_weight=2, ignore=(0,), lost_qubits=None, printing=False, pxs=None, pys=None, pzs=None, pas=None):
     """
     This can take a arbitrary number of target paulis and an arbitrary number of check paulis
+    To make this work for concatenated codes we need to allow px,y,z,a to differ
+    To use for SPF we need to include the error on the output
     """
     if not checks:
         return [1 - no_ec_flip_rate([len(t.support) for t in targets], p) for p in ps], None
@@ -192,95 +264,6 @@ def no_ec_flip_rate(weights, p):
                    range(weight + 1) if k % 2 == 1])
         win *= success_prob
     return 1 - win
-
-
-def best_checks(graph, target, banned_qs=None, printing=False, stab_nt=None, spc=True, total_paulis_done=None, max_overlap=False):
-    """
-    :param graph: graph to calculate stabilizers from
-    :param target: the target operator that we want to check
-    :param banned_qs: if any qubits have been lost, or are the input or outputs they cannot be measured
-    :param printing: print status updates for debugging
-    :param total_paulis_done: If paulis other than the target have been done, the new checks can overlap with these,
-    but must also commute with them. However their overlaps are not as useful as overlaps with the target
-    """
-    if banned_qs is None and spc:
-        banned_qs = [0]
-    elif banned_qs is None:
-        banned_qs = []
-    nq = graph.number_of_nodes()
-    if total_paulis_done is None:
-        total_paulis_done = target
-
-    if printing:
-        print(target.to_str())
-    # find compatible stabilizers
-    if stab_nt is None:
-        gens = stabilizers_from_graph(graph)
-        # We are only interested in the non-trivial stabilizers - trivial stabilizers with non-overlapping supports give us no new info
-        stab_t, stab_nt = gen_stabs_new(gens)
-    compat_stabs = [s for s in stab_nt if (not set(banned_qs).intersection(set(s.support)) and total_paulis_done.commutes_each(s, [i for i in range(nq)]))]
-    if len(compat_stabs) == 0:
-        return []
-
-    # generate a list of all the possible strategies - each strategy may involve multiple stabilizers.
-    num_stabs = len(compat_stabs)
-    stab_dict = {i:compat_stabs[i] for i in range(num_stabs)}
-    input_strats = [(ix,) for ix in range(num_stabs)]
-    all_strats = input_strats[:]
-    set_empty = False
-    while not set_empty:
-        next_set = get_next_possibles(input_strats, num_stabs, stab_dict)
-        if len(next_set) == 0:
-            break
-        else:
-            all_strats += next_set
-            input_strats = next_set
-
-    if max_overlap:
-        # print(max_overlap)
-        current_winner = all_strats[0]
-        best_overlap = 0
-        for b in all_strats:
-            overlap = sum(
-                [len(set(stab_dict[c].support).intersection(target.support)) / len(stab_dict[c].support) for c in b])
-            if overlap > best_overlap:
-                best_overlap = overlap
-                current_winner = b
-                # print(overlap)
-        check = [stab_dict[current_winner[i]] for i in range(len(current_winner))]
-        gen_checks = generating_set(check)
-        # print([c.to_str() for c in gen_checks])
-        # print(best_overlap)
-
-    else:
-    # Get the largest subgroups
-        longest_checklist_ix = np.argmax(np.array([len(i) for i in all_strats]))
-        longest_checks = all_strats[longest_checklist_ix]
-        # Get all the largest subgroups and find the one with best overlap with the target
-        bests = []
-        length = len(longest_checks)
-        if longest_checks is not None:
-            for s in all_strats:
-                if len(s) == length:
-                    bests.append(s)
-            current_winner = bests[0]
-            best_overlap = 0
-            for b in bests:
-                overlap = sum([len(set(stab_dict[c].support).intersection(target.support)) / len(stab_dict[c].support) for c in b])
-                if overlap > best_overlap:
-                    best_overlap = overlap
-                    current_winner = b
-            check = [stab_dict[current_winner[i]] for i in range(len(current_winner))]
-
-            # check = [stab_dict[longest_checks[i]] for i in range(len(longest_checks))]
-            # find a generating set
-            gen_checks = generating_set(check)
-        else:
-            gen_checks = []
-    if printing:
-        print([p.to_str() for p in check])
-        print([p.to_str() for p in gen_checks])
-    return gen_checks
 
 
 def get_next_possibles(n_poss, tot_n_singles, paulis_dict):
@@ -400,24 +383,8 @@ def best_checks_max_clique(graph, target, banned_qs=None, printing=False, stab_n
         print([p.to_str() for p in gen_checks])
     return gen_checks
 
+
 def main():
-    from graphs import gen_ring_graph
-    from pauli_class import Pauli
-    import matplotlib.pyplot as plt
-    import numpy as np
-    ps = np.linspace(0, 0.25)
-    r5 = gen_ring_graph(5)
-    m = Pauli(z_ix=[0], x_ix=[], n=5)
-    tot_pauli = Pauli(z_ix=[0, 2], x_ix=[1], n=5, i_pow=0)
-    print(m.to_str())
-    checks = best_checks(r5, m, spc=False, total_paulis_done=tot_pauli)
-    # checks = [Pauli(x_ix=[1], z_ix=[0, 2], n=5), Pauli(x_ix=[4], z_ix=[0, 3], n=5)]
-    print([c.to_str() for c in checks])
-    # prob, conf = pauli_error_decoder([m], checks, ps, ignore=[])
-    # print(prob)
-    # plt.plot(ps, prob)
-    # plt.plot(ps, (1 - 2 * ps), 'k--')
-    # plt.show()
     pass
 
 
